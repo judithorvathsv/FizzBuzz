30117a994cbfa0206593d5e4f056ed742d67c1f1
Judit Horvath <judit.horvath@appliedtechnology.se>
2024-12-30 16:56:58 +0100

    first commit

---
+++ FizzBuzzCheck.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzCheck
+    {
+        public string GetFizzBuzz(int number){
+
+            return number.ToString();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FizzBuzz;
+
+
+class Program
+{
+    static void Main(string[] args)
+        {
+          
+        if (args.Length > 0 && int.TryParse(args[0], out int number))
+        {
+           
+            var fizzBuzz = new FizzBuzzCheck();            
+           
+            Console.WriteLine(fizzBuzz.GetFizzBuzz(number));
+        }
+        else
+        {
+            Console.WriteLine("Give a valid number.");
+        }
+    }
+
+
+}
